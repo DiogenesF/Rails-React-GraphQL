5374a21fb2b86724dcc48a243110b07d1c0867e3
Diogenes F <diogenesfiorezi@hotmail.com>
2021-04-03 00:06:09 -0300

    mutations added

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -8,10 +8,17 @@ module Types
     # They will be entry points for queries on your schema.
 
     # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :users, [Types::UserType], null: false, description: 'Return all users'
+    def users
+      User.all
+    end
+
+    field :user, Types::UserType, null: false, description: 'Finds an user by a given ID and returns' do
+      argument :id, ID, required: true
+    end
+
+    def user(id:)
+      User.find(id)
     end
   end
 end
--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -5,5 +5,12 @@ module Types
     field :name, String, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+    field :books, [Types::BookType], null: true
+    field :books_count, Integer, null: true
+
+    def books_count
+      object.books.size
+    end
   end
 end
